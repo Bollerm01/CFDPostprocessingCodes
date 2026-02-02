b1e60f33337219d453d8818f1358d13e7d60a5dd
Bollerm01 <bollerm81@gmail.com>
2026-02-02 15:44:52 -0500

    update path

--- TimeMeshSensitivityVolcanoSSWT/volcanoBulkParamExtractor.py
+++ TimeMeshSensitivityVolcanoSSWT/volcanoBulkParamExtractor.py
@@ -14,7 +14,7 @@ import os
 # ============================================================
 
 # --- Input volcano file ---
-VOLCANO_FILE = r"/home/bollerma/LESdata/SSWT/fullCav/meshStudy/test10/test10M2SSWT_000/latest.volcano"
+VOLCANO_FILE = r"/home/bollerma/LESdata/SSWT/fullCav/meshStudy/test11/test11M2SSWT_000/latest.volcano"
 
 # --- Output directory ---
 OUTPUT_DIR = r"/home/bollerma/LESdata/SSWT/fullCav/meshStudy/bulkProbeOutput"
