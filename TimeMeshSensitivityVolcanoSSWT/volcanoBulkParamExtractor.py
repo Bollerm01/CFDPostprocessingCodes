fa4c6b96a4d139feb58e83b0269a4f77b6d064fb
Bollerm01 <bollerm81@gmail.com>
2026-02-11 13:42:36 -0500

    updated for file on Windows test14

--- TimeMeshSensitivityVolcanoSSWT/volcanoBulkParamExtractor.py
+++ TimeMeshSensitivityVolcanoSSWT/volcanoBulkParamExtractor.py
@@ -14,10 +14,12 @@ import os
 # ============================================================
 
 # --- Input volcano file ---
-VOLCANO_FILE = r"/home/bollerma/LESdata/SSWT/fullCav/meshStudy/test13/test13M2SSWT_003/latest.volcano"
+#VOLCANO_FILE = r"/home/bollerma/LESdata/SSWT/fullCav/meshStudy/test13/test13M2SSWT_003/latest.volcano"
+VOLCANO_FILE = r"E:\Boller CFD\AVIATION CFD\MeshSensitivityData\test14M2SSWT_000\checkpoint.1193017.volcano"
 
 # --- Output directory ---
-OUTPUT_DIR = r"/home/bollerma/LESdata/SSWT/fullCav/meshStudy/bulkProbeOutput"
+#OUTPUT_DIR = r"/home/bollerma/LESdata/SSWT/fullCav/meshStudy/bulkProbeOutput"
+OUTPUT_DIR = r"E:\Boller CFD\AVIATION CFD\MeshSensitivityData\bulkFloorCeilingMidlineData\test14"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 # --- Variables to load from the volcano file ---
