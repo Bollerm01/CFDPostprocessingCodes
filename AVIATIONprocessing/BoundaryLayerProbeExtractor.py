95443462c000c60e4ce545892d8329f38393c247
Bollerm01 <bollerm81@gmail.com>
2026-04-15 14:59:16 -0400

    mach

--- AVIATIONprocessing/BoundaryLayerProbeExtractor.py
+++ AVIATIONprocessing/BoundaryLayerProbeExtractor.py
@@ -19,6 +19,7 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 # --- Variables to load from the volcano file ---
 POINT_ARRAYS = [
+    "machnumber",
     "velocitymag",
     "velocitymagavg",
     "velocityx",
