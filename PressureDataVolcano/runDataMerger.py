f473c10b0937bef98fd495020851cfdb7e025426
Bollerm01 <bollerm81@gmail.com>
2026-06-25 10:07:58 -0400

    RD00s

--- PressureDataVolcano/runDataMerger.py
+++ PressureDataVolcano/runDataMerger.py
@@ -5,11 +5,11 @@ import shutil
 # USER INPUTS
 # ==========================
 
-root1 = Path("/data/bollerma/LES/SSWT/sliceCav/RD52s/surfaceData")
-root2 = Path("/data/bollerma/LES/SSWT/sliceCav/RD52s/surfaceData2")
-root3 = Path("/data/bollerma/LES/SSWT/sliceCav/RD52s/surfaceData3")
-root4 = Path("/data/bollerma/LES/SSWT/sliceCav/RD52s/surfaceData4")
-dest_root = Path("/data/bollerma/LES/SSWT/sliceCav/RD52s/surfaceDataCombined")
+root1 = Path("/home/bollerma/LESdata/SSWT/sliceCav/RD00s/surfaceData")
+root2 = Path("/home/bollerma/LESdata/SSWT/sliceCav/RD00s/surfaceData2")
+# root3 = Path("/home/bollerma/LESdata/SSWT/sliceCav/RD00s/surfaceData3")
+# root4 = Path("/home/bollerma/LESdata/SSWT/sliceCav/RD00s/surfaceData4")
+dest_root = Path("/home/bollerma/LESdata/SSWT/sliceCav/RD00s/surfaceDataCombined")
 
 # ==========================
 # COPY FUNCTION
@@ -41,10 +41,10 @@ copy_tree(root1, dest_root)
 print("Copying second dataset (overwriting duplicates)...")
 copy_tree(root2, dest_root)
 
-print("Copying third dataset (overwriting duplicates)...")
-copy_tree(root3, dest_root)
+# print("Copying third dataset (overwriting duplicates)...")
+# copy_tree(root3, dest_root)
 
-print("Copying fourth dataset (overwriting duplicates)...")
-copy_tree(root4, dest_root)
+# print("Copying fourth dataset (overwriting duplicates)...")
+# copy_tree(root4, dest_root)
 
 print("Done.")
